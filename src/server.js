175c6d519b685a0ccc57f359818187847c563ea8
nohj0518 <nohj0518@daum.net>
2024-01-29 23:37:37 +0900

    add express app main handling

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 import express from "express";
-const app = express();
 const PORT = 4000;
+
+const app = express();
+
+const handleHome = () => console.log("Somebody is trying to go home.");
+
+app.get("/", handleHome);
 
 const handleListening = () =>
   console.log(`Hyeonju server listening on port http://localhost:${PORT} 💖`);
