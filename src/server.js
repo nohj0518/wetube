4b0e601452ddeb7937648424c8ec6ab27b120739
nohj0518 <nohj0518@daum.net>
2024-01-30 22:43:15 +0900

    run middleware

--- src/server.js
+++ src/server.js
@@ -3,9 +3,21 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = () => console.log("Somebody is trying to go home.");
+const gossipMiddleware = (req, res, next) => {
+  console.log(`Someone is going to: ${req.url}`);
+  next();
+};
 
-app.get("/", handleHome);
+const handleHome = (req, res, next) => {
+  return res.send("i still love you");
+};
+
+const handleLogin = (req, res) => {
+  return res.send({ message: "login" });
+};
+
+app.get("/", gossipMiddleware, handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () =>
   console.log(`Hyeonju server listening on port http://localhost:${PORT} 💖`);
