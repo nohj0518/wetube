7559a09ff1d8d5e1b4229a344d11fd183ebaf5a9
nohj0518 <nohj0518@daum.net>
2024-02-01 23:15:52 +0900

    add to control middleware

--- src/server.js
+++ src/server.js
@@ -3,8 +3,18 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-  console.log(`Someone is going to: ${req.url}`);
+const logger = (req, res, next) => {
+  console.log(` ${req.method} ${req.url}`);
+  next();
+};
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    console.log("Not Allowed");
+    return res.send("<h1> Not Allowed </h1>");
+  }
+  console.log("Allowed");
   next();
 };
 
@@ -12,12 +22,15 @@ const handleHome = (req, res, next) => {
   return res.send("i still love you");
 };
 
-const handleLogin = (req, res) => {
-  return res.send({ message: "login" });
+const handleProtected = (req, res) => {
+  return res.send({ message: "Welcom to the private lounge." });
 };
 
-app.get("/", gossipMiddleware, handleHome);
-app.get("/login", handleLogin);
+app.use(logger);
+app.use(privateMiddleware);
+
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
   console.log(`Hyeonju server listening on port http://localhost:${PORT} 💖`);
