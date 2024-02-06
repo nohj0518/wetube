2589ae72f94beae7840f2e828cd9fb7b9655d189
nohj0518 <nohj0518@daum.net>
2024-02-07 00:02:24 +0900

    create Controllers and migrate all controllers

---
+++ src/controllers/userController.js
@@ -0,0 +1,3 @@
+export const join = (req, res) => res.send("Join Us");
+export const edit = (req, res) => res.send("Edit me");
+export const remove = (req, res) => res.send("Remove me");
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
