5558933fd49a5045d2dff616a7ad46c2160d0820
nohj0518 <nohj0518@daum.net>
2024-02-18 18:59:34 +0900

    create pugs template and render each pages

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => res.send("Watch");
-export const edit = (req, res) => res.send("Edit");
+export const trending = (req, res) => res.render("home");
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Watch");
 export const upload = (req, res) => res.send("Edit");
 export const deleteVideo = (req, res) => res.send("deleteVideo");
