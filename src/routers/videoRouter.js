50acffe3bbfa7f508e6561de829bca18564e63f7
nohj0518 <nohj0518@daum.net>
2024-02-18 18:59:06 +0900

    modify ids only number using regex

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,8 +4,8 @@ import { upload, see, edit, deleteVideo } from "../controllers/videoController";
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 export default videoRouter;
