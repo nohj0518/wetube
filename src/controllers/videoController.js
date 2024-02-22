63711616122038b3364dc25be07269e58d99285e
nohj0518 <nohj0518@daum.net>
2024-02-22 22:45:31 +0900

    practice to send data to pages with params

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,33 +1,38 @@
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comment: 2,
+    createdAt: "2 min ago",
+    views: 1,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comment: 2,
+    createdAt: "2 min ago",
+    views: 60,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comment: 2,
+    createdAt: "2 min ago",
+    views: 60,
+    id: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "First Video",
-      rating: 5,
-      comment: 2,
-      createdAt: "2 min ago",
-      views: 60,
-      id: 1,
-    },
-    {
-      title: "Second Video",
-      rating: 5,
-      comment: 2,
-      createdAt: "2 min ago",
-      views: 60,
-      id: 2,
-    },
-    {
-      title: "Third Video",
-      rating: 5,
-      comment: 2,
-      createdAt: "2 min ago",
-      views: 60,
-      id: 3,
-    },
-  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("watch", { pageTitle: `Watch ${video.title}`, video });
+};
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Watch");
 export const upload = (req, res) => res.send("Edit");
