702149c215da40e88d4402ee369a700a7a2f72d8
nohj0518 <nohj0518@daum.net>
2024-02-24 19:22:04 +0900

    refactoring video routers

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,16 +1,9 @@
 import express from "express";
-import {
-  upload,
-  watch,
-  edit,
-  deleteVideo,
-} from "../controllers/videoController";
+import { watch, getEdit, postEdit } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
 videoRouter.get("/:id(\\d+)", watch);
-videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 
 export default videoRouter;
