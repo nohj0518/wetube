3336efc78e618f8739e1828d7427c3cf373474d9
nohj0518 <nohj0518@daum.net>
2024-03-03 19:18:12 +0900

    connect to my db

---
+++ src/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://127.0.0.1:27017/wetube");
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Conneted to DB");
+const handleError = (error) => console.log("❌ DB Error", error);
+
+db.on("error", (error) => handleError);
+db.once("open", handleOpen);
