4070edd9f61857f2254a1b9c41e64efd405e4234
nohj0518 <nohj0518@daum.net>
2024-03-03 23:27:14 +0900

    add saving data in db and home going to find db

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,9 @@
-export const home = (req, res) => {
-  return res.render("home", { pageTitle: "Home" });
+import Video from "../models/Video";
+
+export const home = async (req, res) => {
+  const videos = await Video.find({});
+  console.log(videos);
+  return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = (req, res) => {
   const { id } = req.params;
@@ -20,8 +24,17 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: `Upload` });
 };
 
-export const postUpload = (req, res) => {
-  //  we will add a video to the video array
-  const { title } = req.body;
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  await Video.create({
+    title,
+    description,
+    createdAt: Date.now(),
+    hashtags: hashtags.split(",").map((word) => `#${word}`),
+    meta: {
+      views: "0",
+      rating: 0,
+    },
+  });
   return res.redirect(`/`);
 };
