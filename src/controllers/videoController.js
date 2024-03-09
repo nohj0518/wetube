1d7cf1441be3afd8bd8f54a9498f6376b3f33aa7
nohj0518 <nohj0518@daum.net>
2024-03-09 23:27:43 +0900

    modify video controller

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,21 +2,34 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
   const videos = await Video.find({});
-  console.log(videos);
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   const { id } = req.params;
-  return res.render("watch", { pageTitle: `Watch ` });
+  const video = await Video.findById(id);
+  if (!video) return res.render("404", { pageTitle: "Video Not found" });
+  return res.render("watch", { pageTitle: video.title, video });
 };
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-  return res.render("edit", { pageTitle: `Edit` });
+  const video = await Video.findById(id);
+  if (!video) return res.render("404", { pageTitle: "Video Not found" });
+  return res.render("edit", { pageTitle: `Edit`, video });
 };
 
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
+  const { title, description, hashtags } = req.body;
+  const video = await Video.exists({ _id: id });
+  if (!video) return res.render("404", { pageTitle: "Video Not found" });
+
+  await Video.findByIdAndUpdate(id, {
+    title,
+    description,
+    hashtags: hashtags
+      .split(",")
+      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+  });
   return res.redirect(`/videos/${id}`);
 };
 
@@ -30,7 +43,9 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags.split(",").map((word) => `#${word}`),
+      hashtags: hashtags
+        .split(",")
+        .map((word) => (word.startsWith("#") ? word : `#${word}`)),
       meta: {
         views: "0",
         rating: 0,
