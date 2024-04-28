d2caabb8417986e80d53b173027b1990c1b6273f
nohj0518 <nohj0518@daum.net>
2024-04-28 21:03:44 +0900

    sort videos desc in home

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,7 @@
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
-  const videos = await Video.find({});
+  const videos = await Video.find({}).sort({ createdAt: "desc" });
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = async (req, res) => {
