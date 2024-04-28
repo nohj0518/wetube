ddda081d9faf275bedfec67f31fd3682b9c85a2e
nohj0518 <nohj0518@daum.net>
2024-04-28 21:58:12 +0900

    create feat searching by keywords in db

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -64,9 +64,13 @@ export const deleteVideo = async (req, res) => {
 
 export const search = async (req, res) => {
   const { keyword } = req.query;
-  console.log(keyword);
+  let videos = [];
   if (keyword) {
-    // render
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(`${keyword}`, "i"),
+      },
+    });
   }
-  return res.render(`search`, { pageTitle: "Search" });
+  return res.render(`search`, { pageTitle: "Search", videos });
 };
