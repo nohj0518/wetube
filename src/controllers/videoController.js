ec76ffcbee5283f64f77dfd07212dde5225cc2cc
nohj0518 <nohj0518@daum.net>
2024-04-28 19:28:44 +0900

    save

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -26,9 +26,7 @@ export const postEdit = async (req, res) => {
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hashtags: hashtags
-      .split(",")
-      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+    hashtags: Video.formatHashtags(hashtags),
   });
   return res.redirect(`/videos/${id}`);
 };
@@ -43,9 +41,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags
-        .split(",")
-        .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+      hashtags: Video.formatHashtags(hashtags),
       meta: {
         views: "0",
         rating: 0,
